529ee310fae179667dbe89f55e42bc83158f896c
Sreelakshmi-Nanjala <58046900+Sreelakshmi-Nanjala@users.noreply.github.com>
2019-12-19 22:36:14 -0500

    Update cachematrix.R
    committing the cache data assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  set <- function(y){
+    x <<- y
+    inverse  <<- NULL
+  }
+  get <- function() x
+  
+  setinversemean <- function(cachematrix) inverse <<- cachematrix
+  getinversemean <- function() inverse
+  
+  list(set = set, get = get,
+       setinversemean = setinversemean,
+       getinversemean = getinversemean)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinversemean()
+  if(!is.null(inverse)){
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data,...)
+  x$setinversemean(inverse)
+  inverse      
 }
